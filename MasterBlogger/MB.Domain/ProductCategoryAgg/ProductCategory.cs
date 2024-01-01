dcda887ad1c4c0d1570d6185a711bd99f0e1d138
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2024-01-01 17:04:37 +0330

    Init project + MB.Domain class library

---
+++ MasterBlogger/MB.Domain/ProductCategoryAgg/ProductCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.Domain.ProductCategoryAgg
+{
+    public class ProductCategory
+    {
+        public int Id { get; private set; }
+        public string Title { get;private set; }
+        public DateTime CreationDate { get;private set; }
+        public bool IsDeleted { get;private set; }
+
+        public ProductCategory(string title)
+        {
+            Title = title;
+            CreationDate = DateTime.Now;
+            IsDeleted = false;
+        }
+    }
+    
+}
