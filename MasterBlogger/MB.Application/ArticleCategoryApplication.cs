d11e4baeb7e0f5a4fe63b5abfa9b4a45f2261f65
mohammadsadegh mazaheri <sadegh.mazaheri1377@gmail.com>
2024-01-01 17:18:15 +0330

    Add Viewmodel and contract of get all .

---
+++ MasterBlogger/MB.Application/ArticleCategoryApplication.cs
@@ -0,0 +1,18 @@
+﻿using MB.ApplicationContract;
+using MB.ApplicationContract.ArticleCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.Application
+{
+    internal class ArticleCategoryApplication : IArticleCategoryApplication
+    {
+        public List<ArticleCategoryViewModel> List()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MasterBlogger/MB.ApplicationContract/IArticleCategoryApplication.cs
@@ -0,0 +1,14 @@
+﻿using MB.ApplicationContract.ArticleCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.ApplicationContract
+{
+    public interface IArticleCategoryApplication
+    {
+        List<ArticleCategoryViewModel> List();
+    }
+}
